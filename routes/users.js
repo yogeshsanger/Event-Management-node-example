0b588d344e15c55903f7bfe21060aadf586233c4
Yogesh Sanger <yogeshsanger@hotmail.com>
2018-01-09 17:47:54 +0530

    Changes

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', function (req, res, next) {
+    res.send('respond with a resource');
+})
+        .get('/register', function (req, res, next) {
+            res.render('users/register', {title: 'Register New User'});
+        })
+        .post('/register', function (req, res, next) {
+            console.log(req.body);
+            res.send("hello");
+        });
 
 module.exports = router;
